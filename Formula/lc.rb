8dccca77b48acf50dcb28b13981ad18f0a00002c
goreleaserbot <goreleaser@steffen-rumpf.de>
2023-01-04 17:37:11 +0100

    Brew formula update for lc version 0.9

--- Formula/lc.rb
+++ Formula/lc.rb
@@ -5,21 +5,21 @@
 class Lc < Formula
   desc "This tool can be used collect from AWS cloudwatch log groups"
   homepage "https://github.com/steffakasid/lc"
-  version "0.8"
+  version "0.9"
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/steffakasid/lc/releases/download/0.8/lc_0.8_Darwin_arm64.tar.gz"
-      sha256 "0ee0f0d342cb796101533fd86ab8cad14f4674a0c122a58ccfd7fc1b68270277"
+    if Hardware::CPU.intel?
+      url "https://github.com/steffakasid/lc/releases/download/0.9/lc_0.9_Darwin_x86_64.tar.gz"
+      sha256 "08ff8b2de81dcd031f38f75577daf72d057ea70ae610d957855511c83c0e45ba"
 
       def install
         bin.install "lc"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/steffakasid/lc/releases/download/0.8/lc_0.8_Darwin_x86_64.tar.gz"
-      sha256 "07989eadae8731db57cc1259be5ae291abff631f9fe5c92ccb8f0f23eb7b1a01"
+    if Hardware::CPU.arm?
+      url "https://github.com/steffakasid/lc/releases/download/0.9/lc_0.9_Darwin_arm64.tar.gz"
+      sha256 "1684eaa12620ddb4fdbcdd38c12e35ebfb893231c16a3fdbdffd2a6d6edce566"
 
       def install
         bin.install "lc"
@@ -28,17 +28,17 @@ class Lc < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/steffakasid/lc/releases/download/0.8/lc_0.8_Linux_arm64.tar.gz"
-      sha256 "012ac98096099938a33356feb330245804636379c7481a8dc96836cc2e6269a6"
+    if Hardware::CPU.intel?
+      url "https://github.com/steffakasid/lc/releases/download/0.9/lc_0.9_Linux_x86_64.tar.gz"
+      sha256 "337329b590f70777ff6765aac21e75c6f4ec1716670a7bd40ec7af3909274765"
 
       def install
         bin.install "lc"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/steffakasid/lc/releases/download/0.8/lc_0.8_Linux_x86_64.tar.gz"
-      sha256 "9e936d5ec6e732240893bc39a7fa6ce0e417ac1b46fadf9c49d2532899a5d88e"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/steffakasid/lc/releases/download/0.9/lc_0.9_Linux_arm64.tar.gz"
+      sha256 "a7bf4cbf3503bad394b6c6bd2b98844b4ea65474ab081a06abe7ab2fa78b074c"
 
       def install
         bin.install "lc"
